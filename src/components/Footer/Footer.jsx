add70a81d3a2f17a977edcb5bb5fc3638e06fc2c
Manas Vivek Saxena <manasviveksaxena@Manass-MacBook-Pro.local>
2025-05-07 11:26:08 +0530

    Initial it

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css";
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Copyright @ 2025, Cryptoplace - All Rights Reserved.</p>
+      <p>Copyright @ 2025, CryptoPulse X Tracker - All Rights Reserved.</p>
     </div>
   );
 };
